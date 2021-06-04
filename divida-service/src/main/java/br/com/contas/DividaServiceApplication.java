519679522c0db1170a2141795e9c28c9e31265d9
Esley <esleymadeira@gmail.com>
2021-06-03 22:02:15 -0300

    primeiro commit estudo microservicos

---
+++ divida-service/src/main/java/br/com/contas/DividaServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.contas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DividaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DividaServiceApplication.class, args);
+	}
+
+}
